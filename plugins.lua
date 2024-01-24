6ed3325b683ac957a47f89006882e7e32c4d8200
bubuzzz <thaitcao@gmail.com>
2024-01-24 17:32:51 +0700

    first init

---
+++ plugins.lua
@@ -0,0 +1,30 @@
+local plugins = {
+  {"mhinz/vim-startify", lazy=false,},
+  {"dhruvasagar/vim-table-mode", lazy=false,},
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    build = ":Neorg sync-parsers",
+    -- tag = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                work = "~/Dropbox/simplenotes/work",
+                personal = "~/Dropbox/simplenotes/personal",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+}
+
+return plugins
