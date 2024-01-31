f9bc60cdcee35953ca1ce8c4307824ea4d9493e3
bubuzzz <thaitcao@gmail.com>
2024-01-31 13:12:03 +0700

    add markdown preview

--- plugins.lua
+++ plugins.lua
@@ -116,6 +116,13 @@ local plugins = {
       })
     end,
   },
+  {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      ft = { "markdown" },
+      build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
+
 
 return plugins
